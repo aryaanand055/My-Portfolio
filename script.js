8c23acd8fa827a77898f8a86b21e74a6d2f70d5f
aryaanand055 <aryaanand053@gmail.com>
2022-06-15 13:14:46 +0530

    Portfolio

---
+++ script.js
@@ -0,0 +1,57 @@
+const header = document.getElementsByClassName("header")[0];
+const container = document.getElementsByClassName("container")[0];
+header.addEventListener("mouseover", () => {
+    container.style.opacity = 0.4;
+    container.style.backgroundColor = "rgba(255,255,255, 0.1)";
+})
+header.addEventListener("mouseout", () => {
+    container.style.backgroundColor = "var(--main-bg-color)";
+    container.style.opacity = 1;
+})
+
+// Set Inteval for the testimonials shift
+setInterval(() => {
+    plusSlides(1)
+}, 5000)
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+
+
+
+
+document.addEventListener("scroll", () => {
+
+    console.log(window.scrollY)
+    let scroll = window.scrollY;
+    let topEl = document.querySelector(".backToTop");
+    if (scroll > 500) {
+        topEl.classList.remove("d-n")
+    } else {
+        topEl.classList.add("d-n")
+    }
+})
